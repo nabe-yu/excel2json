55c50b385bf62e630e1234698d2e0804490d0618
nabe-yu <yudai.tanabe0528@gmail.com>
2020-10-03 12:31:56 +0900

    edit

--- excel2json.py
+++ excel2json.py
@@ -1,3 +1,8 @@
-import openpyxl
 import pandas as pd
 
+out_path = 'profile.json'
+
+df = pd.read_excel('src_profile.xlsx', sheet_name=0)  # sheet_nameはシート名を文字列あるいはn枚目の整数で指定する
+with open(out_path, 'w', encoding='utf-8') as file:
+    # 文字のエスケープを無効にする
+    df.to_json(file, orient='records', force_ascii=False)
